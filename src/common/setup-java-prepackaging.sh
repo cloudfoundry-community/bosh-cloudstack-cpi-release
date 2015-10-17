105be6b7d845d62504f2a2776aee2a3d90fff0e2
Pierre Oblin <pierre.oblin@orange.com>
2015-10-17 14:05:45 +0200

    fix java8 and mvn archives and version to fit new blobs

--- src/common/setup-java-prepackaging.sh
+++ src/common/setup-java-prepackaging.sh
@@ -1,14 +1,15 @@
-MAVEN_VERSION=apache-maven-3.3.1
+MAVEN_VERSION=apache-maven-3.3.3
+#apache-maven-3.3.3-bin.tar.gz
 JAVA_VERSION=jdk1.8.0_40
-JAVA_TAR_BALL=jdk-8u40-linux-x64.tar.gz
-
+#JAVA_TAR_BALL=jdk-8u40-linux-x64.tar.gz
+JAVA_TAR_BALL=openjdk-1.8.0_40.tar.gz
 cd ${BUILD_DIR}
 
-tar zxf ${BUILD_DIR}/java/${JAVA_TAR_BALL}
+tar zxf ${BUILD_DIR}/java8/${JAVA_TAR_BALL}
 
 export JAVA_HOME=${BUILD_DIR}/${JAVA_VERSION}
 
-tar zxf ${BUILD_DIR}/java/${MAVEN_VERSION}-bin.tar.gz
+tar zxf ${BUILD_DIR}/maven/${MAVEN_VERSION}-bin.tar.gz
 
 export MAVEN_HOME=${BUILD_DIR}/${MAVEN_VERSION}
 export PATH=${MAVEN_HOME}/bin:$PATH
@@ -16,7 +17,8 @@ export PATH=${MAVEN_HOME}/bin:$PATH
 cleanup_java() {
   rm -rf ${JAVA_HOME}
   rm -rf ${MAVEN_HOME}
-  rm -rf ${BUILD_DIR}/java
+  rm -rf ${BUILD_DIR}/java8
+  rm -rf ${BUILD_DIR}/maven
   rm -rf ${BUILD_DIR}/target
   rm -rf ${BUILD_DIR}/common
 }
