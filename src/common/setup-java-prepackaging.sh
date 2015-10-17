24eb180e6c9079d78cb1130dbea9c3f5d2b22c1f
Pierre Oblin <pierre.oblin@orange.com>
2015-10-17 15:21:00 +0200

    use jdk 1.8.0.72, new blob

--- src/common/setup-java-prepackaging.sh
+++ src/common/setup-java-prepackaging.sh
@@ -1,15 +1,17 @@
 MAVEN_VERSION=apache-maven-3.3.3
 #apache-maven-3.3.3-bin.tar.gz
-JAVA_VERSION=jdk1.8.0_40
-#JAVA_TAR_BALL=jdk-8u40-linux-x64.tar.gz
-JAVA_TAR_BALL=openjdk-1.8.0_40.tar.gz
+JAVA_VERSION=jdk1.8.0_72
+JAVA_TAR_BALL=jdk-8u72-ea-bin-b02-linux-x64-13_oct_2015.tar.gz
+
 cd ${BUILD_DIR}
 
-tar zxf ${BUILD_DIR}/java8/${JAVA_TAR_BALL}
+tar zxfv ${BUILD_DIR}/openjdk/${JAVA_TAR_BALL}
 
 export JAVA_HOME=${BUILD_DIR}/${JAVA_VERSION}
+echo $JAVA_HOME
 
-tar zxf ${BUILD_DIR}/maven/${MAVEN_VERSION}-bin.tar.gz
+cd ${BUILD_DIR}
+tar zxfv ${BUILD_DIR}/maven/${MAVEN_VERSION}-bin.tar.gz
 
 export MAVEN_HOME=${BUILD_DIR}/${MAVEN_VERSION}
 export PATH=${MAVEN_HOME}/bin:$PATH
@@ -17,7 +19,7 @@ export PATH=${MAVEN_HOME}/bin:$PATH
 cleanup_java() {
   rm -rf ${JAVA_HOME}
   rm -rf ${MAVEN_HOME}
-  rm -rf ${BUILD_DIR}/java8
+  rm -rf ${BUILD_DIR}/openjdk
   rm -rf ${BUILD_DIR}/maven
   rm -rf ${BUILD_DIR}/target
   rm -rf ${BUILD_DIR}/common
